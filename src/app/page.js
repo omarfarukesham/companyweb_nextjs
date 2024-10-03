f09feda600a36e6d4e1e91211ca230ca564272f8
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-10-03 19:17:06 +0600

    feat: next js project learning

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <h1>Hello test project</h1>
+    
+    </main>
+  );
+}
